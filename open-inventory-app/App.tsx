cf77f2c3514742f12e84ed19ee5e2e70e15263cb
Andreas Houben <andreas.houben@codecentric.de>
2022-01-10 21:00:14 +0100

    Implement Barcode Scanner Demo

--- open-inventory-app/App.tsx
+++ open-inventory-app/App.tsx
@@ -1,12 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import {useEffect, useState} from "react";
+import {BarCodeScannedCallback, BarCodeScanner, BarCodeScannerResult} from "expo-barcode-scanner";
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState<boolean|null>(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const {status} = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({type, data}:BarCodeScannerResult) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (!hasPermission) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <BarCodeScanner
+            onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+            style={StyleSheet.absoluteFillObject}
+        />
+        {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)}/>}
+      </View>
   );
 }
 
